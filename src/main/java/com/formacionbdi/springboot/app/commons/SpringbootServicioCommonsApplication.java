21c196da2de3d4d3b4b8fecf2ab3f3578c2163d1
Saul Avila <saul_harry_@hotmail.com>
2019-07-25 12:08:08 -0500

    se recofnigura el proyecto para excluir la conexion a base de datos

--- src/main/java/com/formacionbdi/springboot/app/commons/SpringbootServicioCommonsApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/commons/SpringbootServicioCommonsApplication.java
@@ -1,6 +1,13 @@
 package com.formacionbdi.springboot.app.commons;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-public class SpringbootServicioCommonsApplication {}
+/**
+ * Se deshabilita la autoconfiguración del datasource
+ */
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
+public class SpringbootServicioCommonsApplication {
+}
