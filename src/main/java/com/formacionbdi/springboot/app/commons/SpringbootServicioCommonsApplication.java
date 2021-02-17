b904d6980ae777cc5eb5d5e49c62136cc2f81c49
Saul Avila <saul.avila@nyrhcloud.com>
2021-02-16 20:16:06 -0600

    Se agregan comentarios para saber a que se refieren algunas cosas.

--- src/main/java/com/formacionbdi/springboot/app/commons/SpringbootServicioCommonsApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/commons/SpringbootServicioCommonsApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 /**
- * Se deshabilita la autoconfiguración del datasource
+ * Se deshabilita la autoconfiguración del datasource esto para poder eliminar
+ * la dependencia de H2, si se quita esto se deberá agregar la dependencia de H2
+ * para evitar cosas raras.
  */
 @EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 public class SpringbootServicioCommonsApplication {
